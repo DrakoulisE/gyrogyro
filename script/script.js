45bcbfff769b1119212aea079d86fddbb21d06cc
DrakoulisE <DrakoulisE@gmail.com>
2025-03-11 03:39:14 +0200

    mobile nav menu

--- script/script.js
+++ script/script.js
@@ -5,6 +5,19 @@ about_button.addEventListener("click" , () => {
     about_target.scrollIntoView({behavior: "smooth"});
 })
 
+const mobile_about_button = document.getElementById("mobile_about_button");
+
+mobile_about_button.addEventListener("click" , () => {
+    about_target.scrollIntoView({behavior: "smooth"});
+    mobile_menu_close.style.display = "none";
+    mobile_menu_open.style.display = "block";
+    mobile_nav_menu.style.display = "none";
+})
+
+about_button.addEventListener("click" , () => {
+    about_target.scrollIntoView({behavior: "smooth"});
+})
+
 const menu_button = document.getElementById("menu_button");
 const menu_target = document.getElementById("menu_target");
 
@@ -12,8 +25,35 @@ menu_button.addEventListener("click" , () => {
     menu_target.scrollIntoView({behavior: "smooth"});
 })
 
+const mobile_menu_button = document.getElementById("mobile_menu_button");
+
+mobile_menu_button.addEventListener("click" , () => {
+    menu_target.scrollIntoView({behavior: "smooth"});
+    mobile_menu_close.style.display = "none";
+    mobile_menu_open.style.display = "block";
+    mobile_nav_menu.style.display = "none";
+})
+
 const logo_button = document.getElementById("logo_button");
 
 logo_button.addEventListener("click" , () => {
     window.scrollTo({top: 0, behavior: "smooth"});
+})
+
+const nav_button_open = document.getElementById("mobile_menu_open");
+
+nav_button_open.addEventListener("click" , () => {
+    let nav_menu = document.getElementById("mobile_nav_menu");
+    nav_menu.style.display = "flex";
+    mobile_menu_open.style.display = "none";
+    mobile_menu_close.style.display = "block";
+})
+
+const nav_button_close = document.getElementById("mobile_menu_close");
+
+nav_button_close.addEventListener("click" , () => {
+    let nav_menu = document.getElementById("mobile_nav_menu");
+    nav_menu.style.display = "none";
+    mobile_menu_open.style.display = "block";
+    mobile_menu_close.style.display = "none";
 })
